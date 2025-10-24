9ceeeaf3378856b2346f78456ef4389a53e77b15
jjmck823 <jjmck823@gmail.com>
2025-10-24 08:30:14 -0400

    added background and shrubs

--- game_background.py
+++ game_background.py
@@ -12,6 +12,12 @@ def make_background():
     water_title_loc= 'assests/background_blocks/tile_0042.png'
     water_tile = pygame.transform.rotozoom(pygame.image.load(water_title_loc), 0, 5)
 
+    shrubs_loc = "assests/background_blocks/tile_0060.png"
+    shrubs = pygame.transform.rotozoom(pygame.image.load(shrubs_loc), 0, 2)
+
+    shrubs_single_loc = "assests/background_blocks/tile_0048.png"
+    shrub_single = pygame.transform.rotozoom(pygame.image.load(shrubs_single_loc), 0, 2)
+
     #get tile width and height 
     tile_width = grass_tile.get_width()
     tile_height = grass_tile.get_height()
@@ -24,14 +30,32 @@ def make_background():
         for y in range(0,HEIGHT,tile_height):
             background.blit(grass_tile, (x,y))
 
-    #make a row of sand 
-    y_sand = HEIGHT - 5 * tile_height
-    for x in range(0, int(WIDTH/2), tile_width):
-        background.blit(sand_top_a, (x, y_sand))
+    #make some sand 
+    #y_sand = HEIGHT - 5 * tile_height
+    #for x in range(0, int(WIDTH/2), tile_width):
+    #    background.blit(sand_top_a, (x, y_sand))
 
-    #make water in top left 
-    x_water = randint(0,WIDTH)
-    Y_water= randint(0,HEIGHT)
-    for y in range(0,WIDTH, tile_width):
-        background.blit(water_tile, (x_water,Y_water))
+
+    #place random shrubs\
+
+    num_shrubs = 8    
+    for i in range(num_shrubs):
+        x = randint(0,WIDTH)
+        y = randint(0,HEIGHT)
+        # blit that seaweed
+        background.blit(shrubs,(x,y))
+    #blit single shrubs 
+
+    num_shrubs_single = 10    
+    for i in range(num_shrubs_single):
+        x = randint(0,WIDTH)
+        y = randint(0,HEIGHT)
+        # blit that seaweed
+        background.blit(shrub_single,(x,y))
+
+
     return background
+
+ 
+    
+
